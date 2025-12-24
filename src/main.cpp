07e0b2447ee719542543abfc89b936b854ec6681
jacobjuneau6 <79861515+jacobjuneau6@users.noreply.github.com>
2025-12-24 09:14:01 -0800

    add autons and opcontrol

--- src/main.cpp
+++ src/main.cpp
@@ -1,54 +1,11 @@
 #include "main.h"
-
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-
-
-/**
- * Runs initialization code. This occurs as soon as the program is started.
- *
- * All other competition modes are blocked by initialize; it is recommended
- * to keep execution time for this mode under a few seconds.
- */
 void initialize() {
 	chassis.initialize();
     chassis.odom_tracker_back_set(&horiz_tracker);
 	lv_example_get_started_3();
 }
-
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
 void disabled() {}
-
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
 void competition_initialize() {}
-
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
 void autonomous() {
 	if (sel == 1 ) {
 		chassis.pid_drive_set(25_in,80);
@@ -123,20 +80,22 @@ void autonomous() {
 		intake2.move(127);
 	}
 }
-
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
 void opcontrol() {
-
+	while (true) {
+		chassis.opcontrol_tank();
+		if (master.get_digital(DIGITAL_L2)) {
+  			intake.move(127);
+  			intake2.move(127);
+		} else if (master.get_digital(DIGITAL_L1)){
+  			intake.move(127);
+		} else {
+  			intake.move(0);
+  			intake2.move(0);
+		}
+		if (master.get_digital(DIGITAL_DOWN)){
+  			intake.move(-127);
+  			intake2.move(-127);
+		}
+		pros::delay(ez::util::DELAY_TIME);  // This is used for timer calculations!  Keep this ez::util::DELAY_TIME
+	}
 }
